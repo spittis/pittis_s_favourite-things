9935a681da9a99774e5a7c6710bd0976db29960c
Soraya Pittis <31996896+spittis@users.noreply.github.com>
2019-03-20 13:15:13 -0400

    starting point

--- routes/index.js
+++ routes/index.js
@@ -12,12 +12,31 @@ connect.query(`SELECT name, avatar FROM hero`, (err, result) => {
     console.log(err);
   } else {
     console.log(result);
-    // res.render('index', { avatars: result });
+    res.render('index', { avatars: result });
   }
   });
 });
 
-module.exports = router;
-
 // want to make a database call to get the image and name, rendering it with the route and putting it in index.js
 // with npm install, you get hte package that lets you communicate with back end --> in terminal, we typed out: npm i -D mysql
+
+
+// get individual data/bio info
+
+router.get('/:hero', function(req, res, next) { //the hero thing doesn't really matter what name it is, it will change to thor, peter, carol or whatever hero //req means request for a resource
+  connect.query(`SELECT * FROM hero WHERE name="${req.params.hero}"`, (err, result) => {
+    if (err) {
+      throw err;
+      console.log(err);
+    } else {
+      console.log(result);
+      res.render('bio', { bioData: result[0] }); //result comes back in an array
+    }
+    });
+  });
+
+
+
+module.exports = router;
+
+
